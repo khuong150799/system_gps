cab80562a5d011e1496d9c2548ff40974a04d04c
khuong150799 <110217462+khuong150799@users.noreply.github.com>
2024-08-24 13:41:08 +0700

    change login logout

--- server.js
+++ server.js
@@ -1,23 +1,53 @@
 const app = require("./src/app");
+const os = require("os");
 const http = require("http");
+const cluster = require("cluster");
 const configureEnvironment = require("./src/config/dotenv.config");
-const process = require("process");
+// const process = require("process");
 
 const { PORT: PORT_ENV, NODE_ENV, DOMAIN_NAME } = configureEnvironment();
 console.log("environment ::::", NODE_ENV);
 
 const PORT = PORT_ENV || 3055;
 
+function init() {
+  if (cluster.isMaster) {
+    let numCPUs = os.cpus().length;
+    // console.log(" Num of CPU ", numCPUs);
+    for (let idx = 1; idx < numCPUs; idx++) {
+      cluster.fork();
+      // let worker = cluster.fork();
+      // worker.on("message", function (msg) {
+      //   console.log("Worker " + msg.worker + " served a " + msg.cmd);
+      //   worker.send("Good work!");
+      // });
+    }
+    cluster.on("exit", (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+      // auto restart worker when worker fail
+      cluster.fork();
+    });
+  } else {
+    startHttpServer();
+  }
+}
 // start server nodejs
-const server = http.createServer(app).listen(PORT, () => {
-  console.log(`Domain server :::: ${DOMAIN_NAME}:${PORT}`);
-});
-
 // const task = require("./src/tasks/issure.task");
-process.on("SIGINT", () => {
-  // task.checkOverload().stop();
-  server.close(() => console.log("Exit server express"));
-  process.exit();
+function startHttpServer() {
+  process.on("message", function (msg) {
+    console.log(msg);
+  });
+  const server = http.createServer(app).listen(PORT, () => {
+    console.log(`Domain server :::: ${DOMAIN_NAME}:${PORT}`);
+  });
 
-  // notify send (ping....)
-});
+  process.on("SIGINT", () => {
+    // task.checkOverload().stop();
+    server.close(() => console.log("Exit server express"));
+    process.exit();
+
+    // notify send (ping....)
+  });
+}
+
+init();
